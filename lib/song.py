ab0ca57a91b6004e51e751ccd46d654aef33db78
Wambui <nderituwambui93@gmail.com>
2024-06-20 02:02:42 +0300

    updating labs

--- lib/song.py
+++ lib/song.py
@@ -1,2 +1,21 @@
 class Song:
-    pass
+
+    count = 0
+    genres = ["Rap", "Pop", "Rock"]
+    artists = ["Jay Z", "Beyonce", "Nirvana", "Hall and Oates"]
+    genre_count = {}
+    artist_count = {}
+
+    def __init__(self, name, artist, genre):
+        self.name = name
+        self.artist = artist
+        self.genre = genre
+        Song.count += 1
+        if genre not in Song.genre_count:
+            Song.genre_count[genre] = 1
+        else:
+            Song.genre_count[genre] += 1
+        if artist not in Song.artist_count:
+            Song.artist_count[artist] = 1
+        else:
+            Song.artist_count[artist] += 1
